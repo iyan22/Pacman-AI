2aacbcfc7219c064527c9e1990b9cce9e281b8dc
Iyán Álvarez <iyan.22.alvarez@gmail.com>
2022-03-14 09:41:11 +0100

    PL2 initial commit - Updated gitignore, added statement and code

---
+++ code/pl2/projectParams.py
@@ -0,0 +1,8 @@
+# projectParams.py
+# ----------------
+
+
+STUDENT_CODE_DEFAULT = 'naiveBayes.py,perceptron.py,mira.py,dataClassifier.py,answers.py,perceptron_pacman.py'
+PROJECT_TEST_CLASSES = 'classificationTestClasses.py'
+PROJECT_NAME = 'Project 5: Classification'
+BONUS_PIC = False
